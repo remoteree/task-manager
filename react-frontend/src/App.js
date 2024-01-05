7a15e312e371a453e611519df947f64d1c9705c9
reshadbinharun <reshadbinharun@gmail.com>
2024-01-05 10:07:13 -0600

    working frontend

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,29 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import Counter from './components/Counter';
+import Layout from './components/Layout';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    if (count > 10) {
-      setMessage('Count is greater than 10');
-    } else {
-      setMessage('');
-    }
-  }, [count]);
-
-
-  const handleCountChange = (newCount) => {
-    setCount(newCount);
-  };
   return (
-    <div>
-      {message && <p>{message}</p>}
-      <Counter
-        onCountChange={handleCountChange}
-      />
-    </div>
+    <Layout/>
   );
 }
 
---
+++ react-frontend/src/components/HistoryView.js
@@ -0,0 +1,25 @@
+import { Segment, Card, Button } from 'semantic-ui-react';
+
+// History View Component
+const HistoryView = ({ summaries }) => {
+    return <Segment.Group>
+        <Segment>
+        {
+            summaries.map(summary => (
+            <Card key={summary.date}>
+            <Card.Content>
+                <Card.Header>{summary.date}</Card.Header>
+                <Card.Meta>Tasks completed: {summary.completed}</Card.Meta>
+                <Card.Description>Time worked: {summary.hoursWorked} hours</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <Button basic onClick={() => {}}>View Detailed Analytics</Button>
+            </Card.Content>
+            </Card>))
+        }
+        </Segment>
+        
+        </Segment.Group>
+}
+
+export default HistoryView;
